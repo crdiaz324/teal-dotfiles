983e595a843a8b1ef7a32a865a0b4a4fb3df70a1
Carlos Diaz <cdiaz@Carloss-Macbook-Pro.local>
2023-06-29 22:46:23 -0700

    inital commit

---
+++ .config/nvim/lua/custom/init.lua
@@ -0,0 +1,31 @@
+-- runs in the main init.lua, its meant to have vim options, globals, autocmds, commands, etc.
+vim.opt.colorcolumn = "90"
+
+-- Alwasy have 8 lines of text before the end of the screen
+vim.o.scrolloff = 8 
+
+-- Make realative line numbers default 
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
+
+-- Case insesitive searching UNLESS /C or capital in search
+vim.o.completeopt = 'menuone,noselect'
+
+-- [[ Highlight on yank ]]
+-- See ':help vim.highlight.on_yank()'
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function ()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+M.general = {
+  v = {
+    ["J"] = {
+      ":m '>+1<CR>gv=gv",
+      "Move visual selection down", 
+      opts = {nowait = true}},
+    ["K"] = {":m '<-2<CR>gv=gv",
+      "Move visual selection up",
+      opts = {nowait = true}},
+    ["gc"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "Toggle comment",
+    },
+  },
+  n = {
+    ["gc"] = {
+      function ()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "Toggle comment",
+    },
+    ["<c-i>"] = {
+      function ()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+  }
+}
+
+M.dap = {
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Toggle Breakpoint"
+    },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.crates = {
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "Update crates"
+    }
+  }
+}
+return M

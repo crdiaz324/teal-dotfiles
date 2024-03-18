c5f8832c5527a84217f9e0320e8f2da7f5b6ea6c
Carlos Diaz <crdiaz324@gmail.com>
2024-03-18 09:01:46 -0700

    updated keymaps

--- .config/LazyVim/lua/config/keymaps.lua
+++ .config/LazyVim/lua/config/keymaps.lua
@@ -5,6 +5,11 @@
 -- Disable LSP
 vim.keymap.set("n", "<leader>S", "<cmd>LspStop<CR>", { desc = "LspStop" })
 
+-- Use blackhole register for cw or C type commands
+vim.keymap.set("n", "cw", '"_cw', { desc = "cw to blackhole register" })
+vim.keymap.set("n", "c", '"_c', { desc = "cw to blackhole register" })
+vim.keymap.set("n", "C", '"_C', { desc = "cw to blackhole register" })
+
 -- initialize global var to false -> nvim-cmp turned off per default
 vim.g.cmptoggle = false
 
